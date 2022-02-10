257dd4de4b89b526985390ad4822c2a49b2e2002
mdrumon42243 <mdrumonkhan48519@gmail.com>
2022-02-10 20:48:07 +0600

    Alhamdulillah all done

--- js/banking.js
+++ js/banking.js
@@ -1,9 +1,32 @@
+
 const depositButton = document.getElementById('deposit-button');
 const depositInput = document.getElementById('deposit-input');
-const depositSum = document.getElementById('deposit-sum');
+const depositValue = document.getElementById('deposit');
+const blance = document.getElementById('blance');
+const withdraw = document.getElementById('withdraw');
+const withdrawButton = document.getElementById('withdraw-button');
+const withdrawInput = document.getElementById('withdraw-input');
+
+// Deposit value setting 
 depositButton.addEventListener('click', function () {
     const depositInputValue = depositInput.value;
-    depositSum.innerText = depositInputValue;
+    const newDepositInputSum = parseFloat(depositInputValue);
+    const depositValueText = depositValue.innerText;
+    const depositSum = parseFloat(depositValueText);
+    if (depositInput.value == '') {
+        alert("Please enter a valid number!!!")
+    }
+    depositValue.innerText = depositSum + newDepositInputSum;
+    const oldestBlance = parseFloat(blance.innerText);
+    blance.innerText = oldestBlance + newDepositInputSum;
     depositInput.value = '';
+});
 
-})
+withdrawButton.addEventListener('click', function () {
+    const withdrawText = parseFloat(withdraw.innerText);
+    const MyBlance = parseFloat(blance.innerText);
+    const withdrawInputValue = parseFloat(withdrawInput.value);
+    withdraw.innerText = withdrawText + withdrawInputValue;
+    blance.innerText = MyBlance - withdrawInputValue;
+    withdrawInput.value = '';
+});
--- js/login.js
+++ js/login.js
@@ -1,36 +1,13 @@
-
-
-
-
-
-// const userEmail = document.getElementById('user-email');
-// const userPassword = document.getElementById('user-password');
-// const loginButton = document.getElementById('login-button');
-
-// const userEmailValue = userEmail.value;
-// const userPasswordValue = userPassword.value;
-
-// loginButton.addEventListener('click', function () {
-//     if (userEmailValue == 'mdrumonkhan48519@gmail.com' && userPasswordValue == 'khan143') {
-//         window.location.href = 'newpage.html';
-//     }
-// });
-
-
-
+// Bank login form
 document.getElementById('login-button').addEventListener('click', function () {
     const userEmail = document.getElementById('user-email');
     const userEmailValue = userEmail.value;
-
     const userPassword = document.getElementById('user-password');
     const userPasswordValue = userPassword.value;
-
-
     if (userEmailValue == 'khan@rumon.com' && userPasswordValue == 'khan') {
         window.location.href = 'newpage.html';
     }
     else {
         alert('Please enter  the valid number!!!')
     }
-
 });
